aca6471d3b663e983dbc045e60147cae4514fb7a
Javier Quevedo <jquevedo@gmail.com>
2018-08-13 13:47:01 +0200

    Fixed typo in comment

--- example/index.js
+++ example/index.js
@@ -4,7 +4,7 @@ import HeatMap from '../src';
 
 const xLabels = new Array(24).fill(0).map((_, i) => `${i}`);
 
-// Display only EVENT labels
+// Display only even labels
 const xLabelsVisibility = new Array(24).fill(0).map((_, i) => i % 2 === 0 ? true : false);
 
 const yLabels = ['Sun', 'Mon', 'Tue'];
