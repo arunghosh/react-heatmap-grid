c88ee28a940a42e9ac0bf36219216bb9be87bd8e
zT0Tz <tzhan320@uwo.ca>
2019-01-17 15:31:11 -0500

    keep clean

--- example/index.js
+++ example/index.js
@@ -30,7 +30,7 @@ ReactDOM.render(
       background: `rgb(66, 86, 244, ${1 - (max - value) / (max - min)})`,
       fontSize: "11px",
     })}
-    cellRender={(value, x, y) => value && `${value}%`}
+    cellRender={value => value && `${value}%`}
   />,
   document.getElementById("app")
 );
